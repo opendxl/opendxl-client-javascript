fc1cc0cf1258b87bf1f47d0263fd72eb1866c92f
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-12-19 17:31:01 -0800

    Add exports for npm package

---
+++ dxl.js
@@ -0,0 +1,14 @@
+'use strict'
+
+exports.Broker = require('./lib/broker')
+exports.Client = require('./lib/client')
+exports.Config = require('./lib/config')
+exports.DxlError = require('./lib/dxl_error')
+exports.ErrorResponse = require('./lib/error_response')
+exports.Event = require('./lib/event')
+exports.MalformedBrokerError = require('./lib/malformed_broker_error')
+exports.Message = require('./lib/message')
+exports.Request = require('./lib/request')
+exports.Response = require('./lib/response')
+exports.ServiceRegistrationInfo = require('./lib/service_registration_info')
+exports.Util = require('./lib/util')
