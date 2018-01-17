13057039b46b63e6b66cc333589af36d57902e74
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-01-17 12:22:35 -0800

    Add message 'other fields' integration test

---
+++ test/integration/message_other_fields_test.js
@@ -0,0 +1,32 @@
+'use strict'
+/* eslint no-unused-expressions: "off" */ // for chai expect assertions
+
+var expect = require('chai').expect
+var Event = require('../../lib/event')
+var util = require('../../lib/util')
+var TestClient = require('./test_client')
+
+describe('"other fields" in an event message @integration', function () {
+  it('should be delivered with the expected content', function (done) {
+    var otherFieldsCount = 1000
+
+    var client = new TestClient(this, done)
+    client.connect(function () {
+      var topic = 'message_other_fields_test_' + util.generateIdAsString()
+
+      var eventToSend = new Event(topic)
+      for (var i = 0; i < otherFieldsCount; i++) {
+        eventToSend.otherFields['key' + i] = 'value' + i
+      }
+
+      client.addEventCallback(topic, function (eventReceived) {
+        client.shutdown(null, function () {
+          expect(eventReceived.otherFields).to.eql(eventToSend.otherFields)
+          done()
+        })
+      })
+
+      client.sendEvent(eventToSend)
+    })
+  })
+})
