9b5f807bb535ad9f48212a545b163e51e5aa2bc4
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-01-17 15:34:53 -0800

    Add ServiceRegistrationInfo tests

---
+++ test/service_registration_info_test.js
@@ -0,0 +1,79 @@
+'use strict'
+/* eslint no-unused-expressions: "off" */ // for chai expect assertions
+
+var expect = require('chai').expect
+var ServiceRegistrationInfo = require('../lib/service_registration_info')
+var TestClient = require('./integration/test_client')
+
+describe('ServiceRegistrationInfo', function () {
+  var client = new TestClient(this)
+  context('constructor', function () {
+    it('should store the parameter as the service type', function () {
+      var expectedServiceType = 'my service type'
+      var serviceInfo = new ServiceRegistrationInfo(client, expectedServiceType)
+      expect(serviceInfo.serviceType).to.equal(expectedServiceType)
+    })
+  })
+
+  context('.addTopic', function () {
+    it('should store a single topic and callback', function () {
+      var serviceInfo = new ServiceRegistrationInfo(client, 'my service type')
+      var topic = 'my topic'
+      var callback = function () {}
+
+      expect(serviceInfo.topics).to.eql([])
+      expect(serviceInfo.callbacks(topic)).to.eql([])
+
+      serviceInfo.addTopic(topic, callback)
+
+      expect(serviceInfo.topics).to.eql([topic])
+      expect(serviceInfo.callbacks(topic)).to.eql([callback])
+    })
+
+    it('should store multiple topics and callbacks', function () {
+      var serviceInfo = new ServiceRegistrationInfo(client, 'my service type')
+      var topic1 = 'my topic 1'
+      var callback1 = function () {}
+      var topic2 = 'my topic 2'
+      var callback2 = function () {}
+
+      serviceInfo.addTopic(topic1, callback1)
+      expect(serviceInfo.callbacks(topic2)).to.eql([])
+
+      serviceInfo.addTopic(topic2, callback2)
+      expect(serviceInfo.topics.sort()).to.eql([topic1, topic2].sort())
+      expect(serviceInfo.callbacks(topic1)).to.eql([callback1])
+      expect(serviceInfo.callbacks(topic2)).to.eql([callback2])
+    })
+
+    it('should store multiple callbacks for the same topic', function () {
+      var serviceInfo = new ServiceRegistrationInfo(client, 'my service type')
+      var topic = 'my topic'
+      var callback1 = function () {}
+      var callback2 = function () {}
+
+      serviceInfo.addTopic(topic, callback1)
+      serviceInfo.addTopic(topic, callback2)
+      expect(serviceInfo.topics).to.eql([topic])
+      expect(serviceInfo.callbacks(topic)).to.eql([callback1, callback2])
+    })
+  })
+
+  context('.addTopics', function () {
+    it('should store multiple topics and callbacks', function () {
+      var serviceInfo = new ServiceRegistrationInfo(client, 'my service type')
+      var sharedCallback = function () {}
+      var topicsToCallbacks = {
+        topic1: sharedCallback,
+        topic2: function () {},
+        topic3: sharedCallback
+      }
+
+      serviceInfo.addTopics(topicsToCallbacks)
+      expect(serviceInfo.topics.sort()).to.eql(Object.keys(topicsToCallbacks))
+      expect(serviceInfo.callbacks('topic1')).to.eql([topicsToCallbacks.topic1])
+      expect(serviceInfo.callbacks('topic2')).to.eql([topicsToCallbacks.topic2])
+      expect(serviceInfo.callbacks('topic3')).to.eql([topicsToCallbacks.topic3])
+    })
+  })
+})
