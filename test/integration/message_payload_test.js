7dfd9045f9e380503a88805ab0cbba699e0d2b42
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-01-19 10:39:22 -0800

    Added non-ASCII character message payload test

--- test/integration/message_payload_test.js
+++ test/integration/message_payload_test.js
@@ -9,60 +9,89 @@ var Request = require('../../lib/request')
 var ServiceRegistrationInfo = require('../../lib/service_registration_info')
 var util = require('../../lib/util')
 var TestClient = require('./test_client')
+var testHelpers = require('../test_helpers')
 
 describe('message payload in an async request @integration', function () {
-  it('should be delivered with the expected content', function (done) {
-    var expectedString = 'SslUtils'
-    var expectedInt = 123456
+  context('with buffers of various sizes', function () {
+    it('should be delivered with the expected content', function (done) {
+      var expectedString = 'SslUtils'
+      var expectedInt = 123456
 
-    var requestsSent = {}
-    var requestsReceived = {}
-    var requestCount = 3
+      var requestsSent = {}
+      var requestsReceived = {}
+      var requestCount = 3
 
-    var client = new TestClient(this, done)
-    client.connect(function () {
-      var topic = 'message_payload_test_' + util.generateIdAsString()
-      var regInfo = new ServiceRegistrationInfo(client,
-        'message_payload_test_service')
+      var client = new TestClient(this, done)
+      client.connect(function () {
+        var topic = 'message_payload_test_buffers_' + util.generateIdAsString()
+        var regInfo = new ServiceRegistrationInfo(client,
+          'message_payload_test_buffers_service')
 
-      regInfo.addTopic(topic, function (request) {
-        if (requestsSent[request.messageId]) {
-          requestsReceived[request.messageId] = request
-          if (Object.keys(requestsReceived).length === requestCount) {
-            client.shutdown(null, function () {
-              Object.keys(requestsReceived).forEach(function (messageId) {
-                var payload = new BufferList(
-                  requestsReceived[messageId].payload
-                )
-                expect(msgpack.decode(payload)).to.equal(expectedString)
-                expect(msgpack.decode(payload)).to.eql(
-                  requestsSent[messageId].expectedBuffer
-                )
-                expect(msgpack.decode(payload)).to.equal(expectedInt)
+        regInfo.addTopic(topic, function (request) {
+          if (requestsSent[request.messageId]) {
+            requestsReceived[request.messageId] = request
+            if (Object.keys(requestsReceived).length === requestCount) {
+              client.shutdown(null, function () {
+                Object.keys(requestsReceived).forEach(function (messageId) {
+                  var payload = new BufferList(
+                    requestsReceived[messageId].payload
+                  )
+                  expect(msgpack.decode(payload)).to.equal(expectedString)
+                  expect(msgpack.decode(payload)).to.eql(
+                    requestsSent[messageId].expectedBuffer
+                  )
+                  expect(msgpack.decode(payload)).to.equal(expectedInt)
+                })
+                done()
               })
-              done()
-            })
+            }
           }
+        })
+        client.registerServiceAsync(regInfo)
+
+        for (var index = 0; index < requestCount; index++) {
+          var request = new Request(topic)
+          var expectedBuffer = Buffer.alloc(Math.pow(2, index * 8) + 1, index + 1)
+          var bufferList = new BufferList()
+          bufferList.append(msgpack.encode(expectedString))
+          bufferList.append(msgpack.encode(expectedBuffer))
+          bufferList.append(msgpack.encode(expectedInt))
+          request.payload = bufferList.slice()
+
+          requestsSent[request.messageId] = {
+            request: request,
+            expectedBuffer: expectedBuffer
+          }
+
+          client.asyncRequest(request)
         }
       })
-      client.registerServiceAsync(regInfo)
+    })
+  })
+  context('with non-ASCII character in a string', function () {
+    it('should be delivered with the expected context', function (done) {
+      var expectedString = 'hello \ud83d\ude39'
 
-      for (var index = 0; index < requestCount; index++) {
+      var client = new TestClient(this, done)
+      client.connect(function () {
+        var topic = 'message_payload_test_non_ascii_' +
+          util.generateIdAsString()
+        var regInfo = new ServiceRegistrationInfo(client,
+          'message_payload_test_non_ascii_service')
+
+        regInfo.addTopic(topic, function (request) {
+          client.shutdown(null, function () {
+            expect(testHelpers.decodePayload(request)).to.equal(expectedString)
+            done()
+          })
+        })
+
+        client.registerServiceAsync(regInfo)
+
         var request = new Request(topic)
-        var expectedBuffer = Buffer.alloc(Math.pow(2, index * 8) + 1, index + 1)
-        var bufferList = new BufferList()
-        bufferList.append(msgpack.encode(expectedString))
-        bufferList.append(msgpack.encode(expectedBuffer))
-        bufferList.append(msgpack.encode(expectedInt))
-        request.payload = bufferList.slice()
-
-        requestsSent[request.messageId] = {
-          request: request,
-          expectedBuffer: expectedBuffer
-        }
-
+        request.payload = expectedString
         client.asyncRequest(request)
-      }
+      })
     })
   })
 })
