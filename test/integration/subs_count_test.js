5c991fd8545bef1d7fe956278893287b2b229abb
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-01-19 15:22:03 -0800

    Rework subscription tracking in subs_count_test
    Previously, the subs_count_test waited for the subacks received by the
    client to equal the number of subscriptions sent before testing the
    subscription count with the broker. subacks for internal topics that the
    client could subscribe for, though, could cause the test to ask the
    broker for the number of subscriptions prematurely. This commit reworks
    the logic to ensure that all of the subacks for the topics that the test
    is specifically subscribing for have been received before testing the
    subscription count with the broker.

--- test/integration/subs_count_test.js
+++ test/integration/subs_count_test.js
@@ -11,16 +11,35 @@ describe('broker subs count @integration', function () {
   it('should return expected values for topic subscriptions', function (done) {
     var maxTestTime = 5000
 
+    var baseTopic = 'subs_count_test'
     var random1 = util.generateIdAsString()
     var random2 = util.generateIdAsString()
-    var topic1 = 'subs_count_test/foo/' + random1 + '/' + random2
-    var topic2 = 'subs_count_test/bar/' + random2
+    var topic1 = baseTopic + '/foo/' + random1 + '/' + random2
+    var topic2 = baseTopic + '/bar/' + random2
 
-    var clientsToConnect = 7
+    // Each item in this array corresponds to a client connection which should
+    // be established and a subarray of topic names to which the client should
+    // subscribe.
+    var topicsByClient = [
+      [],
+      [topic1],
+      [topic1, topic2],
+      [topic1, topic2],
+      [baseTopic + '/foo/' + random1 + '/#'],
+      [baseTopic + '/+/' + random1 + '/#'],
+      ['#']
+    ]
+
+    var clientsToConnect = topicsByClient.length
+    var subscriptionsToAttempt = topicsByClient.reduce(function (acc, current) {
+      return acc + current.length
+    }, 0)
+    var subscriptionAttemptsSent = 0
+    var subscriptionMessagesToBeAcked = 0
+    var infoByClient = []
+
     var connectedClients = 0
-    var topicsToSubscribe = 8
-    var subscriptionsComplete = 0
-    var clients = []
+    var subscriptionMessagesAcked = 0
 
     var subsRequest = function (topic) {
       var request = new Request('/mcafee/service/dxl/broker/subs')
@@ -30,14 +49,16 @@ describe('broker subs count @integration', function () {
 
     var testTimeout = null
 
+    // Iterate over all of the clients which have been created, shutting down
+    // each one.
     var terminateClients = function (callback) {
       clearTimeout(testTimeout)
-      var clientCount = clients.length
+      var clientsToShutdown = infoByClient.length
       var clientsShutdown = 0
-      clients.forEach(function (client) {
-        client.shutdown(null, function () {
+      infoByClient.forEach(function (info) {
+        info.client.shutdown(null, function () {
           clientsShutdown++
-          if (clientCount === clientsShutdown) {
+          if (clientsToShutdown === clientsShutdown) {
             callback()
           }
         })
@@ -55,45 +76,89 @@ describe('broker subs count @integration', function () {
       )
     }, maxTestTime)
 
-    for (var i = 0; i < clientsToConnect; i++) {
-      clients[i] = new TestClient(this)
-      clients[i].on('packetreceive', function (packet) {
-        if ((typeof packet !== 'undefined') && (packet.cmd === 'suback')) {
-          subscriptionsComplete++
-          if (subscriptionsComplete === topicsToSubscribe) {
-            testHelpers.asyncRequest(clients[0], subsRequest(topic1),
-              terminateClientsWithError,
-              function (topic1Response) {
-                testHelpers.asyncRequest(clients[0], subsRequest(topic2),
-                  terminateClientsWithError,
-                  function (topic2Response) {
-                    terminateClients(function () {
-                      expect(testHelpers.jsonPayloadToObject(
-                        topic1Response).count).to.equal(6)
-                      expect(testHelpers.jsonPayloadToObject(
-                        topic2Response).count).to.equal(3)
-                      done()
-                    })
-                  }
-                )
-              }
-            )
-          }
-        }
+    topicsByClient.forEach(function (topics) {
+      var client = new TestClient(this)
+      infoByClient.push({
+        client: client,
+        topics: topics,
+        subscriptionMessageIds: []
       })
-      clients[i].connect(function () {
+      client.connect(function () {
         connectedClients++
         if (connectedClients === clientsToConnect) {
-          clients[1].subscribe(topic1)
-          clients[2].subscribe(topic1)
-          clients[3].subscribe(topic1)
-          clients[4].subscribe('subs_count_test/foo/' + random1 + '/#')
-          clients[5].subscribe('subs_count_test/+/' + random1 + '/#')
-          clients[2].subscribe(topic2)
-          clients[3].subscribe(topic2)
-          clients[6].subscribe('#')
+          infoByClient.forEach(function (clientInfo) {
+            // Listen for packets being sent in order to determine the point
+            // at which all intended subscriptions have been sent to the broker.
+            // The message id for each subscription attempt is captured so
+            // that it can be matched up to the messageId on the corresponding
+            // subscription ack (suback) from the broker.
+            clientInfo.client.on('packetsend', function (packet) {
+              if (packet.cmd === 'subscribe') {
+                // The client could batch one or more topics together
+                // into the same subscription packet so iterate through each
+                // one of the available topics to count them.
+                packet.subscriptions.forEach(function (subscription) {
+                  var currentSubscriptionAttemptsSent = subscriptionAttemptsSent
+                  // Only count this subscription if this is for a topic
+                  // that this test specifically subscribed for - i.e., not
+                  // the topic that the client subscribes itself for in order
+                  // to receive responses to requests that it makes.
+                  if ((subscription.topic.indexOf(baseTopic) === 0) ||
+                    (subscription.topic === '#')) {
+                    subscriptionAttemptsSent++
+                  }
+                  if (currentSubscriptionAttemptsSent <
+                    subscriptionAttemptsSent) {
+                    clientInfo.subscriptionMessageIds.push(packet.messageId)
+                    subscriptionMessagesToBeAcked++
+                  }
+                })
+              }
+            })
+            // Listen for packets being received in order to determine when
+            // all subscription attempts have been acked from the broker.
+            clientInfo.client.on('packetreceive', function (packet) {
+              var subscriptionMessageIds = clientInfo.subscriptionMessageIds
+              if ((packet.cmd === 'suback') &&
+                (subscriptionMessageIds.indexOf(packet.messageId) >= 0)) {
+                subscriptionMessagesAcked++
+                if ((subscriptionAttemptsSent === subscriptionsToAttempt) &&
+                  (subscriptionMessagesAcked ===
+                    subscriptionMessagesToBeAcked)) {
+                  // Subscriptions have all been acked by the broker at this
+                  // point so it should be safe to query the broker to
+                  // determine how many subscriptions it is holding for the
+                  // topics that this test uses.
+                  var requestClient = infoByClient[0].client
+                  testHelpers.asyncRequest(requestClient, subsRequest(topic1),
+                    terminateClientsWithError,
+                    function (topic1Response) {
+                      testHelpers.asyncRequest(requestClient,
+                        subsRequest(topic2),
+                        terminateClientsWithError,
+                        function (topic2Response) {
+                          terminateClients(function () {
+                            expect(testHelpers.jsonPayloadToObject(
+                              topic1Response).count).to.equal(6)
+                            expect(testHelpers.jsonPayloadToObject(
+                              topic2Response).count).to.equal(3)
+                            done()
+                          })
+                        }
+                      )
+                    }
+                  )
+                }
+              }
+            })
+            // Clients were all successfully connected so send subscriptions
+            // for topics from the appropriate clients.
+            clientInfo.topics.forEach(function (topic) {
+              clientInfo.client.subscribe(topic)
+            })
+          })
         }
       })
-    }
+    })
   })
 })
